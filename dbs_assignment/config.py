385f7c4a96d7228978e3a98c5004a9ac07af861a
Marek Smetanka <marek.smetanka1@gmail.com>
2023-02-19 19:49:31 +0100

    wip: test status

--- dbs_assignment/config.py
+++ dbs_assignment/config.py
@@ -5,8 +5,8 @@ load_dotenv()
 class Settings(BaseSettings):
 
     DATABASE_USER: str = os.getenv("DATABASE_USER", "postgres")
-    DATABASE_PASSWORD: str = os.getenv("DATABASE_PASSWORD", "tajneheslo")
-    DATABASE_HOST: str = os.getenv("DATABASE_HOST", "localhost")
+    DATABASE_PASSWORD: str = os.getenv("DATABASE_PASSWORD", "postgres")
+    DATABASE_HOST: str = os.getenv("DATABASE_HOST", "127.0.0.1")
     DATABASE_PORT: str = os.getenv("DATABASE_PORT", 5432)  # default postgres port is 5432
     DATABASE_NAME: str = os.getenv("DATABASE_NAME", "postgres")
     DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
