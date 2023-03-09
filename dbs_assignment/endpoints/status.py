b0b1908bba5a65eefc4502bd826432d12e9124d0
Marek Smetanka <smetmarek@gmail.com>
2023-03-09 14:07:45 +0100

    wip: create endpoints

--- dbs_assignment/endpoints/status.py
+++ dbs_assignment/endpoints/status.py
@@ -2,18 +2,20 @@ from fastapi import APIRouter
 
 from dbs_assignment.config import settings
 import psycopg2
+import json
 
 router = APIRouter()
-conn = psycopg2.connect(
-    user=settings.DATABASE_USER,
-    password=settings.DATABASE_PASSWORD,
-    host=settings.DATABASE_HOST,
-    port=settings.DATABASE_PORT,
-    database=settings.DATABASE_NAME)
 
 
 @router.get("/v1/status")
 async def connect():
+    conn = psycopg2.connect(
+        user=settings.DATABASE_USER,
+        password=settings.DATABASE_PASSWORD,
+        host=settings.DATABASE_HOST,
+        port=settings.DATABASE_PORT,
+        database=settings.DATABASE_NAME)
+
     curr = conn.cursor()
     curr.execute("SELECT version();")
     version = curr.fetchall()
@@ -25,10 +27,16 @@ async def connect():
 
 @router.get("/v1/passengers/{id}/companions")
 async def connect(id):
+    conn = psycopg2.connect(
+        user=settings.DATABASE_USER,
+        password=settings.DATABASE_PASSWORD,
+        host=settings.DATABASE_HOST,
+        port=settings.DATABASE_PORT,
+        database=settings.DATABASE_NAME)
     curr = conn.cursor()
     curr.execute("\
-        SELECT json_build_object('id', t2.passenger_id, 'name', t2.passenger_name,\
-       'flights_count', COUNT(flights.flight_id), 'flights', json_agg(flights.flight_id))\
+        SELECT (json_build_object('id', t2.passenger_id, 'name', t2.passenger_name,\
+       'flights_count', COUNT(flights.flight_id), 'flights', json_agg(flights.flight_id)))\
         FROM bookings.flights\
         JOIN bookings.ticket_flights t on flights.flight_id = t.flight_id\
         JOIN bookings.tickets t2 on t.ticket_no = t2.ticket_no\
@@ -40,11 +48,186 @@ async def connect(id):
         WHERE tickets.passenger_id = (%s))\
         GROUP BY t2.passenger_id, t2.passenger_name", (id,))
 
-    result = curr.fetchall()
-    data = []
-    for r in result:
-        data.append(r)
+    data = curr.fetchall()
+    result = []
+    for json in data:
+        result.append(json[0])
 
     return {
         'results': result
     }
+@router.get("/v1/bookings/{id}")
+async def connect(id):
+    conn = psycopg2.connect(
+        user=settings.DATABASE_USER,
+        password=settings.DATABASE_PASSWORD,
+        host=settings.DATABASE_HOST,
+        port=settings.DATABASE_PORT,
+        database=settings.DATABASE_NAME)
+    curr = conn.cursor()
+    curr.execute("\
+           SELECT json_build_object(\
+               'id', bookings.book_ref,\
+               'book_date', book_date,\
+               'boarding_passes', (SELECT json_agg(json_build_object(\
+                'id', t.passenger_id,\
+                'passenger_id', t.passenger_name,\
+                'passenger_name', fl.flight_no,\
+                'boarding_no',  bp.boarding_no,\
+                'flight_no', fl.flight_no,\
+                'seat', bp.seat_no,\
+                'aircraft_code',  fl.aircraft_code,\
+                'arrival_airport', fl.arrival_airport,\
+                'departure_airport', fl.departure_airport,\
+                'scheduled_arrival', fl.scheduled_arrival,\
+                'scheduled_departure', fl.scheduled_departure\
+                )\
+            ))\
+           )\
+    FROM bookings\
+            LEFT JOIN tickets t ON bookings.book_ref = t.book_ref\
+            LEFT JOIN ticket_flights tf ON t.ticket_no = tf.ticket_no\
+            LEFT JOIN boarding_passes bp ON tf.ticket_no = bp.ticket_no\
+            LEFT JOIN flights fl ON bp.flight_id = fl.flight_id\
+    WHERE bookings.book_ref = %s\
+    GROUP BY bookings.book_ref", (id,))
+
+    data = curr.fetchall()
+    result = []
+    for json in data:
+        result.append(json[0])
+
+    return {
+        'results': result
+    }
+
+@router.get("/v1/flights/late-departure/{delay}")
+async def connect(delay):
+    conn = psycopg2.connect(
+        user=settings.DATABASE_USER,
+        password=settings.DATABASE_PASSWORD,
+        host=settings.DATABASE_HOST,
+        port=settings.DATABASE_PORT,
+        database=settings.DATABASE_NAME)
+    curr = conn.cursor()
+    curr.execute("\
+           SELECT EXTRACT(MINUTES FROM (actual_departure  - scheduled_departure)) as delay,\
+            json_build_object(\
+                'flight_id', flight_id,\
+                'flight_no', flight_no,\
+                'delay', EXTRACT(MINUTES FROM (actual_departure  - scheduled_departure))\
+            )\
+            FROM flights\
+            WHERE EXTRACT(MINUTES FROM (actual_departure  - scheduled_departure)) >= %s\
+            ORDER BY delay DESC", (delay,))
+
+    data = curr.fetchall()
+    result = []
+    for json in data:
+        result.append(json[1])
+
+    return {
+        'results': result
+    }
+
+@router.get("/v1/top-airlines")
+async def connect(limit):
+   return limit
+@router.get("/v1/departures")
+async def connect(airport, day):
+    conn = psycopg2.connect(
+        user=settings.DATABASE_USER,
+        password=settings.DATABASE_PASSWORD,
+        host=settings.DATABASE_HOST,
+        port=settings.DATABASE_PORT,
+        database=settings.DATABASE_NAME)
+    curr = conn.cursor()
+    curr.execute("\
+          SELECT EXTRACT(week FROM scheduled_departure),\
+            json_build_object(\
+               'flight_id', flight_id,\
+               'flight_no', flight_no,\
+               'scheduled_departure', scheduled_departure\
+            )\
+            FROM flights\
+            WHERE departure_airport = %s\
+            AND EXTRACT(week FROM scheduled_departure) = %s\
+            GROUP BY flight_id\
+            ORDER BY scheduled_departure", (airport, day, ))
+
+    data = curr.fetchall()
+    result = []
+    for json in data:
+        result.append(json[1])
+
+    return {
+        'results': result
+    }
+
+@router.get("/v1/airports/{airport}/destination")
+async def connect(airport):
+    conn = psycopg2.connect(
+        user=settings.DATABASE_USER,
+        password=settings.DATABASE_PASSWORD,
+        host=settings.DATABASE_HOST,
+        port=settings.DATABASE_PORT,
+        database=settings.DATABASE_NAME)
+    curr = conn.cursor()
+    curr.execute("\
+          SELECT\
+            airport_code\
+        FROM airports_data\
+        LEFT JOIN flights f on airports_data.airport_code = f.arrival_airport\
+        WHERE f.departure_airport = %s\
+        GROUP BY airport_code\
+        ORDER BY airport_code", (airport, ))
+
+    data = curr.fetchall()
+    result = []
+    for json in data:
+        result.append(json[0])
+
+    return {
+        'results': result
+    }
+
+@router.get("/v1/airlines/{flight_no}/load")
+async def connect(flight_no):
+    conn = psycopg2.connect(
+        user=settings.DATABASE_USER,
+        password=settings.DATABASE_PASSWORD,
+        host=settings.DATABASE_HOST,
+        port=settings.DATABASE_PORT,
+        database=settings.DATABASE_NAME)
+    curr = conn.cursor()
+    curr.execute("\
+          SELECT\
+            json_build_object(\
+               'id', flights.flight_id,\
+               'aircraft_capacity', COUNT(s.seat_no),\
+               'load', COUNT(ticket_no),\
+               'percentage', ROUND(100 - ((COUNT(seat_no)/100) - (COUNT(ticket_no)/100)) * 100, 1)\
+            )\
+        FROM flights\
+        LEFT JOIN seats s ON flights.aircraft_code = s.aircraft_code\
+        LEFT JOIN ticket_flights tf ON flights.flight_id = tf.flight_id\
+        WHERE flights.flight_no = %s\
+        GROUP BY flights.flight_id\
+        ORDER BY flights.flight_id", (flight_no, ))
+
+    data = curr.fetchall()
+    result = []
+    for json in data:
+        result.append(json[0])
+
+    return {
+        'results': result
+    }
+
+@router.get("/v1/airlines/{flight_no}/load-week")
+async def connect(flight_no):
+
+    return {
+        'results': "result"
+    }
+
